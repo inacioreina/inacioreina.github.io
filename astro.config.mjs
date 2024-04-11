c09a52f84e80707576386be3bcac634522c234c4
inacioreina <inacioreina@gmail.com>
2024-04-11 15:39:47 +0100

    config and deploy setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,7 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fuwari.vercel.app/',
+  site: 'https://inacioreina.github.io/',
   base: '/',
   integrations: [
     tailwind(),
